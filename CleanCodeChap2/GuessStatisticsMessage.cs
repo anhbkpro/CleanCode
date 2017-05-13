9fd6349112ab8ab839873ff0042393d4648fd607
anhbkpro <laingocanh1990@gmail.com>
2017-05-13 13:48:36 +0700

    second commit

---
+++ CleanCodeChap2/GuessStatisticsMessage.cs
@@ -0,0 +1,53 @@
+﻿namespace CleanCodeChap2
+{
+    internal sealed class GuessStatisticsMessage
+    {
+        string _number;
+        string _verb;
+        string _pluralModifier;
+
+
+        public string Make(string candidate, int count)
+        {
+            CreatePluralDependentMessageParts(count);
+            return $"There {_verb} {_number} {candidate}{_pluralModifier}";
+        }
+
+        public void CreatePluralDependentMessageParts(int count)
+        {
+            switch (count)
+            {
+                case 0:
+                    ThereAreNoLetters();
+                    break;
+                case 1:
+                    ThereIsOneLetter();
+                    break;
+                default:
+                    ThereAreMannyLetters(count);
+                    break;
+            }
+        }
+
+        private void ThereAreMannyLetters(int count)
+        {
+            _number = count.ToString();
+            _verb = "are";
+            _pluralModifier = "s";
+        }
+
+        private void ThereIsOneLetter()
+        {
+            _number = "1";
+            _verb = "is";
+            _pluralModifier = "";
+        }
+
+        private void ThereAreNoLetters()
+        {
+            _number = "no";
+            _verb = "are";
+            _pluralModifier = "s";
+        }
+    }
+}
---
+++ CleanCodeChap2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CleanCodeChap2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = new GuessStatisticsMessage();
+            Console.WriteLine(message.Make("backend developer", 0));
+        }
+    }
+}
